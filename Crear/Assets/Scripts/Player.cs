38a2049be84fd0f3ca5f5cac3540e1fe4ad8cc34
jmuleiro <joaquinmuleirobeltran@gmail.com>
2021-03-27 16:45:46 -0300

    unity ?? modified file again

--- Crear/Assets/Scripts/Player.cs
+++ Crear/Assets/Scripts/Player.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : CustomMonoBehaviour
 {
-
+    public GameObject interactionKey;
+    private GameObject npcInRange;
+    private SpriteRenderer interactionKeyRenderer;
     public Vector2 speed = new Vector2(2, 2);
     public Rigidbody2D rb;
+
+    private void Start()
+    {
+        logPrefix = "Player >> ";
+        npcInRange = new GameObject("null");
+        interactionKeyRenderer = interactionKey.GetComponent<SpriteRenderer>();
+        interactionKeyRenderer.enabled = false;
+    }
 
     void handleMovement()
     {
@@ -29,8 +39,33 @@ public class Player : MonoBehaviour
         transform.Translate(movement);
     }
 
+    void triggerInteractionKey(GameObject npc = null)
+    {
+        interactionKeyRenderer.enabled = (npc.name != "null");
+        npcInRange = npc;
+    }
+
+    void handleInteraction()
+    {
+        if (interactionKeyRenderer.enabled == true &&
+        Input.GetKeyDown(KeyCode.E) &&
+        npcInRange.name != "null")
+        {
+            npcInRange.SendMessage("toggleDialog");
+            consoleLog("handleInteraction", "passed");
+        }
+
+        /*if (npcInRange == null)
+            consoleLog(nameof(npcInRange), "null");
+        else
+            consoleLog(nameof(npcInRange), npcInRange.name);*/
+
+    }
+
     void Update()
     {
         handleMovement();
+        handleInteraction();
     }
+
 }
